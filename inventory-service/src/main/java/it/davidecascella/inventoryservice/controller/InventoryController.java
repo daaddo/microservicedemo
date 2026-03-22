a7b88b8b4c66086af85c40a00525efaf83ca29ed
DaviDio <david.cascella.5@gmail.com>
2026-03-22 13:52:14 +0100

    fix .properties db config and added controller

--- inventory-service/src/main/java/it/davidecascella/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/it/davidecascella/inventoryservice/controller/InventoryController.java
@@ -1,5 +1,7 @@
 package it.davidecascella.inventoryservice.controller;
 
+import it.davidecascella.inventoryservice.repository.InventoryItemRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,13 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/inventory")
-
+@RequiredArgsConstructor
 public class InventoryController {
+
+    private final InventoryItemRepository inventoryItemRepository;
 
     @PreAuthorize("permitAll()")
     @GetMapping("/check")
     public ResponseEntity<Boolean> isStockAvailable(@RequestParam String skuCode, @RequestParam Integer qty) {
         log.info("skuCode={}, quantity={}", skuCode, qty);
-        return ResponseEntity.ok().body(true);
+        boolean available = inventoryItemRepository.findBySkuCode(skuCode)
+                .map(item -> item.getQuantity() >= qty)
+                .orElse(false);
+        return ResponseEntity.ok().body(available);
     }
 }
